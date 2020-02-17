88adba93fc0a84d8c8fd6af93615dd1f9c73b1e7
ANSHAJ KUMAR <55790104+anshajsharma@users.noreply.github.com>
2020-02-17 20:12:42 +0530

    Pdf and Image Sending Done

--- src/main/java/com/example/chatapp/Messaging/SingleMessage.java
+++ src/main/java/com/example/chatapp/Messaging/SingleMessage.java
@@ -2,8 +2,83 @@ package com.example.chatapp.Messaging;
 
 public class SingleMessage {
 
-    String message_body, type , delivery_status , sender , receiver , image_url ,video_url , avaibility ;
+    String message_body, type , delivery_status , sender , receiver , image_url ,video_url , avaibility ,downloaded , file_name ,file_url;
     long timestamp , sent_timestamp , received_timestamp , seen_timestamp ;
+
+    public SingleMessage(String message_body, String type, String delivery_status, String sender, String receiver, String image_url, String video_url, String avaibility, String downloaded, String file_name, String file_url, long timestamp, long sent_timestamp, long received_timestamp, long seen_timestamp) {
+        this.message_body = message_body;
+        this.type = type;
+        this.delivery_status = delivery_status;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.image_url = image_url;
+        this.video_url = video_url;
+        this.avaibility = avaibility;
+        this.downloaded = downloaded;
+        this.file_name = file_name;
+        this.file_url = file_url;
+        this.timestamp = timestamp;
+        this.sent_timestamp = sent_timestamp;
+        this.received_timestamp = received_timestamp;
+        this.seen_timestamp = seen_timestamp;
+    }
+
+    public SingleMessage(String message_body, String type, String delivery_status, String sender, String receiver, String image_url, String video_url, String avaibility, String downloaded, String file_name, long timestamp, long sent_timestamp, long received_timestamp, long seen_timestamp) {
+        this.message_body = message_body;
+        this.type = type;
+        this.delivery_status = delivery_status;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.image_url = image_url;
+        this.video_url = video_url;
+        this.avaibility = avaibility;
+        this.downloaded = downloaded;
+        this.file_name = file_name;
+        this.timestamp = timestamp;
+        this.sent_timestamp = sent_timestamp;
+        this.received_timestamp = received_timestamp;
+        this.seen_timestamp = seen_timestamp;
+    }
+
+    public String getFile_url() {
+        return file_url;
+    }
+
+    public void setFile_url(String file_url) {
+        this.file_url = file_url;
+    }
+
+    public String getFile_name() {
+        return file_name;
+    }
+
+    public void setFile_name(String file_name) {
+        this.file_name = file_name;
+    }
+
+    public SingleMessage(String message_body, String type, String delivery_status, String sender, String receiver, String image_url, String video_url, String avaibility, String downloaded, long timestamp, long sent_timestamp, long received_timestamp, long seen_timestamp) {
+        this.message_body = message_body;
+        this.type = type;
+        this.delivery_status = delivery_status;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.image_url = image_url;
+        this.video_url = video_url;
+        this.avaibility = avaibility;
+        this.downloaded = downloaded;
+        this.timestamp = timestamp;
+        this.sent_timestamp = sent_timestamp;
+        this.received_timestamp = received_timestamp;
+        this.seen_timestamp = seen_timestamp;
+    }
+
+    public String getDownloaded() {
+        return downloaded;
+    }
+
+    public void setDownloaded(String downloaded) {
+        this.downloaded = downloaded;
+    }
 
     public SingleMessage(String message_body, String type, String delivery_status, String sender, String receiver, String image_url, String video_url, long timestamp, long sent_timestamp, long received_timestamp, long seen_timestamp, String avaibility) {
         this.message_body = message_body;
--- src/main/java/com/example/chatapp/SettingsActivity.java
+++ src/main/java/com/example/chatapp/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.example.chatapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -25,6 +28,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Callback;
@@ -152,88 +156,148 @@ public class SettingsActivity extends AppCompatActivity {
     // Whole work of profile changing done here.. database... imageshow... links... all handled
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
+
                 final Uri resultUri = result.getUri();
-                final Uri resultUri2 = data.getData();
 
+                imageCompressorAndUploader(resultUri);
 
-                // Time stampGenerator.... best way to get unique id....
-                // Nothing can be better than this in uniqueness
-                String timeStamp = String.valueOf(TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
-
-                //mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
-                final String curr_uid = mCurrentUser.getUid();
-
-                final StorageReference filePath = mProfilePictures.child("profile_pictures").child(curr_uid).child("profile_picture.jpg");
-
-                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                       if(task.isSuccessful()){
-
-                           filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                               @Override
-                               public void onSuccess(Uri uri) {
-                                   String profilePicUrl = uri.toString();
-                                   File imageFile = new File(resultUri.getPath());
-                                   try {
-                                       compressImageFile = new Compressor(SettingsActivity.this)
-                                               .setMaxHeight(200)
-                                               .setMaxWidth(200)
-                                               .setQuality(15)
-                                               .compressToBitmap(imageFile);
-                                   } catch (IOException e) {
-                                       e.printStackTrace();
-                                   }
-                                   //    Toast.makeText(SettingsActivity.this, profilePicUrl, Toast.LENGTH_SHORT).show();
-                                   Log.i(TAG, "onSuccess: "+ profilePicUrl);
-
-
-                              // Thumbnail==Profile Imahe Compression upload done here
-
-                                   ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                                   compressImageFile.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                                   byte[] thumbData = baos.toByteArray();
-
-                                   final StorageReference thumbFileRef = mProfilePictures.child("profile_pictures").child(curr_uid).child("thumb_nail.jpg");
-
-                                   UploadTask uploadTask = thumbFileRef.putBytes(thumbData);
-                                   uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                                       @Override
-                                       public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                                       }
-                                   })
-                                     .addOnFailureListener(new OnFailureListener() {
-                                         @Override
-                                         public void onFailure(@NonNull Exception e) {
-                                             Toast.makeText(SettingsActivity.this, "Thumb_image compression failed!", Toast.LENGTH_SHORT).show();
-                                         }
-                                     });
-
-
-
-
-                                   mDatabaseRef.child("image").setValue(profilePicUrl);
-                                   mDatabaseRef.keepSynced(true);
-                               }
-                           });
-                           Toast.makeText(SettingsActivity.this, "Successful", Toast.LENGTH_SHORT).show();
-
-                       }
-                       else{
-                           Toast.makeText(SettingsActivity.this, "Failed!!", Toast.LENGTH_SHORT).show();
-
-                       }
-                    }
-                });
 
 
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
             }
+        }
+    }
+
+    private void imageCompressorAndUploader(final Uri resultUri) {
+
+        //private Bitmap compressImageFile; ---------- initialise it on top of class or activity
+        //final Uri resultUri = data.getData(); ----- initialise it in onActivityResult
+
+        if(resultUri!=null) {
+
+
+            // Time stampGenerator.... best way to get unique id....
+            // Nothing can be better than this in uniqueness
+            String timeStamp = String.valueOf(TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
+
+
+            FirebaseUser mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+            final String curr_uid = mCurrentUser.getUid();
+
+            final DatabaseReference mDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Users").child(curr_uid);
+
+            final StorageReference filePath = mProfilePictures.child("profile_pictures").child(curr_uid).child("profile_picture.jpg");
+
+            final UploadTask uploadTask = filePath.putFile(resultUri);
+//              final UploadTask  uploadTask = filePath.putFile(resultUri);
+
+            final ProgressDialog mProgress = new ProgressDialog(SettingsActivity.this);
+            mProgress.setTitle("Uploading...");
+
+
+            mProgress.setCancelable(true);
+
+            mProgress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            mProgress.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    uploadTask.cancel();
+                }
+            });
+
+            //  mProgress.setInverseBackgroundForced(setColorFilter(0x80000000, PorterDuff.Mode.MULTIPLY));
+            mProgress.show();
+
+            uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if (task.isSuccessful()) {
+
+                        filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                String profilePicUrl = uri.toString();
+                                if (resultUri.getPath() != null) {
+                                    File imageFile = new File(resultUri.getPath());
+                                    try {
+                                        compressImageFile = new Compressor(getApplicationContext())
+                                                .setMaxHeight(200)
+                                                .setMaxWidth(200)
+                                                .setQuality(15)
+                                                .compressToBitmap(imageFile);
+                                    } catch (IOException e) {
+
+                                        e.printStackTrace();
+                                    }
+                                    //    Toast.makeText(SettingsActivity.this, profilePicUrl, Toast.LENGTH_SHORT).show();
+                                    Log.i("fuygih", "onSuccess: " + profilePicUrl);
+
+
+                                    // Thumbnail==Profile Imahe Compression upload done here
+
+                                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                    compressImageFile.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                                    byte[] thumbData = baos.toByteArray();
+
+                                    final StorageReference thumbFileRef = mProfilePictures.child("profile_pictures").child(curr_uid).child("thumb_nail.jpg");
+
+                                    UploadTask uploadTask1 = thumbFileRef.putBytes(thumbData);
+                                    uploadTask1.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                                        }
+                                    })
+                                            .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                                    if (task.isSuccessful()) {
+                                                        thumbFileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                            @Override
+                                                            public void onSuccess(Uri uri) {
+                                                                mDatabaseRef.child("thumb_nail").setValue(uri.toString());
+                                                            }
+                                                        });
+
+                                                    }
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(getApplicationContext(), "Thumb_image compression failed!", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+
+
+                                    mDatabaseRef.child("image").setValue(profilePicUrl);
+                                    // mDatabaseRef.keepSynced(true);
+                                }
+
+                            }
+                        });
+                        Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Failed!!", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            })
+            .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    mProgress.setMessage("Uploaded: " + (int) progress + "%");
+                    mProgress.setProgress((int) progress);
+                }
+            });
         }
     }
 }
